84525e49052b291ab3e535947dcbfd2b1ad6e96d
Oliver White <whiteoliverg@gmail.com>
2022-08-27 18:02:25 -0400

    better handle dark mode flashing

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import { useLocalStorage } from '@mantine/hooks';
 import { AppProps } from 'next/app';
 import styled from '@emotion/styled';
 import { theme } from '../styles/theme';
+import { useEffect } from 'react';
 
 const Layout = styled('div')`
     position: relative;
@@ -19,9 +20,15 @@ const Layout = styled('div')`
 `;
 
 const Root = ({ Component, pageProps }: AppProps) => {
+    let darkTheme = false;
+    useEffect(function onFirstMount() {
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        darkTheme = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    }, []);
+
     const [colorScheme, setColorScheme] = useLocalStorage<ColorScheme>({
         key: 'mantine-color-scheme',
-        defaultValue: 'light',
+        defaultValue: darkTheme ? 'dark' : 'light',
     });
 
     const toggleColorScheme = (value?: ColorScheme) => {
@@ -63,6 +70,36 @@ const Root = ({ Component, pageProps }: AppProps) => {
                                     : theme.white,
                         },
                     })}
+                />
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+              (function() {
+                window.__onThemeChange = function() {};
+                function setTheme(newTheme) {
+                  window.__theme = newTheme;
+                  preferredTheme = newTheme;
+                  document.body.className = newTheme;
+                  window.__onThemeChange(newTheme);
+                }
+                var preferredTheme;
+                try {
+                  preferredTheme = localStorage.getItem('theme');
+                } catch (err) { }
+                window.__setPreferredTheme = function(newTheme) {
+                  setTheme(newTheme);
+                  try {
+                    localStorage.setItem('theme', newTheme);
+                  } catch (err) {}
+                }
+                var darkQuery = window.matchMedia('(prefers-color-scheme: dark)');
+                darkQuery.addListener(function(e) {
+                  window.__setPreferredTheme(e.matches ? 'dark' : 'light')
+                });
+                setTheme(preferredTheme || (darkQuery.matches ? 'dark' : 'light'));
+              })();
+            `,
+                    }}
                 />
                 <Layout>
                     <Component {...pageProps} />
