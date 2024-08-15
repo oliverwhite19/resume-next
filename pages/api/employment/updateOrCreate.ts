17635d1faf6724cc3a4ba640979287ac61448ad5
Oliver White <whiteoliverg@gmail.com>
2024-08-15 18:05:18 -0400

    authorize creation

--- pages/api/employment/updateOrCreate.ts
+++ pages/api/employment/updateOrCreate.ts
@@ -1,11 +1,16 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { PrismaClient } from '../../../prisma/generated/client';
+import { authorizeAdmin } from '../../../centreware/authentication';
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
   const prisma = new PrismaClient();
+
+  if (!authorizeAdmin(req, res)) {
+    return;
+  }
 
   const { id, name, link, descriptor } = req.body;
 
