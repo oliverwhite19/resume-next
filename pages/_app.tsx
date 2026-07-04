48226c999182b9bfd3e75fbe8f01699e34a7cea8
Oliver White <whiteoliverg@gmail.com>
2026-07-04 19:39:57 -0400

    Add missing Mantine core styles import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import { MantineProvider } from '@mantine/core';
+import '@mantine/core/styles.css';
 import { AppProps } from 'next/app';
 import { theme } from '../styles/theme';
 import Head from 'next/head';
