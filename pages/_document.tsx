e7d1cab8655ac2eef77086b9a8a8142e2f3de6a9
Oliver White <whiteoliverg@gmail.com>
2026-07-05 12:45:59 -0400

    Add SEO meta tags, canonical URLs, and JSON-LD structured data

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,7 +7,6 @@ export default class _Document extends Document {
         <Head>
           <meta charSet="utf-8" />
           <link rel="icon" href="/favicon.ico" />
-          <meta name="description" content="My resume and projects website" />
           <link rel="apple-touch-icon" href="/logo192.png" />
         </Head>
         <body>
